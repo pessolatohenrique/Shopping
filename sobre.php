ee41678f86071a88dc92b42a6dc7320cd24e1253
Henrique Pessolato <pessolatohenrique@gmail.com>
2017-02-19 12:18:42 -0300

    Página Sobre o Shopping

---
+++ sobre.php
@@ -0,0 +1,34 @@
+<?php require_once("include/cabecalho.php"); ?>
+<main>
+	<h1>Conheça o shopping</h1>
+	<div class="container">
+		<section class="textoShopping">
+			<h2>O Shopping</h2>
+			<p>
+				O complexo faz parte do mais novo bairro da cidade de São Caetano do Sul, o espaço dos <strong>designers</strong> - uma área de 480 mil m<sup>2</sup>, totalmente planejada e sustentável, que reúne moradia, trabalho e lazer.
+			</p>
+			<p>
+				O Shopping possui <em>318</em> lojas em 3 andares, <em>5.102</em> vagas de estacionamento, em <em>104,9</em> mil m<sup>2</sup> de área construída e <em>39</em> mil m<sup>2</sup> de área bruta locável.
+			</p>
+		</section>
+		<section class="arquitetura">
+			<h2>Projeto Arquitetônico</h2>
+			<p>
+				Projetado e assinado por <strong>Ted Mosby</strong>, o prédio possui um layout <em>moderno</em>, <em>adaptável</em> a diferentes públicos. Transmite tranquilidade, de modo a simular um jardim ao ar livre, garantindo a total segurança. Projeto com total <em>sustentabilidade</em>, associando tecnologia, simplicidade e uso de energia sem afetar ao meio-ambiente.
+			</p>
+		</section>
+		<section class="projetosSociais">
+			<h2>Projetos Sociais</h2>
+			<p>
+				O nosso shopping é totalmente adepto a práticas e projetos sociais, no qual possa colaborar com toda a população. Alguns de nossos projetos sociais envolvem:
+				<ul>
+					<li>Contribuição de renda</li>
+					<li>Doação a empresas de diferentes finalidades</li>
+					<li>Apoio a diversidade</li>
+					<li>Apoio a educação, cultura e lazer</li>
+				</ul>
+			</p>
+		</section>
+	</div>
+</main>
+<?php require_once("include/rodape.php");
